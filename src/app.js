69a075cb01fa13dce28423082c2d9b4a026dd2b3
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-02 07:00:47 +0000

    Arrays looping

--- src/app.js
+++ src/app.js
@@ -14,13 +14,20 @@
 // }
 
 //Print even numbers from 0 to 20
-for(let i=0;i<=20;i++){
-    if(i%2 == 0){
-        console.log(i);
-    }
-}
+// for(let i=0;i<=20;i++){
+//     if(i%2 == 0){
+//         console.log(i);
+//     }
+// }
 
-//reverse count from 100 to 10(with 10 decrement)
-for(let i=100; i>=0;i-=10){
-    console.log(i);
+// //reverse count from 100 to 10(with 10 decrement)
+// for(let i=100; i>=0;i-=10){
+//     console.log(i);
+// }
+
+//Looping over arrays - aka iterating over an array
+const colors = ["voilet","indigo","blue","green","yellow","orange","red"];
+
+for(let i=0; i<colors.length;i++){
+    console.log(i, colors[i]);
 }
